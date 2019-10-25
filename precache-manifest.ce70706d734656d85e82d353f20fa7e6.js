4d757401ea387d836612a6ff4c454eafaff99a7e
mrcoding <39582062+MingRay98@users.noreply.github.com>
2019-10-25 16:59:09 +0800

    Updates

---
+++ precache-manifest.ce70706d734656d85e82d353f20fa7e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf4c036b25fef674da713ffb3d18d9",
+    "url": "/webcam/index.html"
+  },
+  {
+    "revision": "30ecb3da53a0d6964c9b",
+    "url": "/webcam/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "20d88a2b89e081d7877e",
+    "url": "/webcam/static/js/2.96f98a65.chunk.js"
+  },
+  {
+    "revision": "30ecb3da53a0d6964c9b",
+    "url": "/webcam/static/js/main.5a589bf1.chunk.js"
+  },
+  {
+    "revision": "f855aaebee04e1fd6f11",
+    "url": "/webcam/static/js/runtime-main.9b368f1f.js"
+  }
+]);
